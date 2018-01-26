c53992d9bcda0060462dad8ac4237bf02a76f6b2
Rémi LEBLOND <leblond.remi@gmail.com>
2018-01-26 17:52:26 +0100

    Ajout d'un script d'extraction de la liste des clubs photos de l'UR21 de la FPF

---
+++ listeClubUR21.py
@@ -0,0 +1,51 @@
+#!/usr/bin/python3
+# coding: utf-8
+#----------------------------------------------------------
+# Récupération de la liste des clubs photos de l'UR21
+#----------------------------------------------------------
+from urllib.request import urlopen
+from bs4 import BeautifulSoup
+
+# Analyse de la page
+def chercheClub(page):
+    for tableau in page.find_all("table", attrs={'class':'spip'}):
+        # print (tableau)
+        for tbody in tableau.find_all("tbody"):
+            # print (tbody)
+            for ligne in tbody.find_all("tr"):
+                nbCol = 1
+                for col in ligne.find_all("td"):
+                    if (nbCol == 2) :
+                        nomClub = col.string
+                    if (nbCol == 4) :
+                        villeClub = col.string
+                    if (nbCol == 5) :
+                        try:
+                            emailClub = col.find("a").get('title').replace('..åt..', '@')
+                        except:
+                            emailClub = ""
+                        
+                    if (nbCol == 6) :
+                        try:
+                            lienClub = col.find("a").get('href')
+                        except:
+                            lienClub = ""
+                        print ("<li><b>%s</b> à %s. Adresse de contact : %s <a href='%s'>%s</a></li>"%(nomClub, villeClub, emailClub, lienClub, lienClub))
+                        # print (emailClub)
+                    
+                    nbCol = nbCol+1
+        #             numClub = col[0]
+        #             print ("- %s" % numClub)
+        # # refNom = club.find('div', attrs={'class':'title'}).string.strip()
+        # refUrl = club.find('div', attrs={'class':'link'}).find('a').string.strip()
+
+        # print ("- %s (%s)" % (refNom, refUrl))
+
+url = "http://ur21.federation-photo.fr/photo-clubs/"
+try:
+    with urlopen(url) as response:
+        webPage = BeautifulSoup(response.read(), "html.parser")
+        chercheClub(webPage)
+except urllib.error.URLError:
+    print ("Impossible d'ouvrir la page %s" % url)
+    exit()
